ef9a0d9138c5e2a6718c99b2d4afbdfc2a4d3d3b
孙棋 <1604492745@qq.com>
2019-03-08 16:17:05 +0800

    的的2

--- src/main/java/com/tianxia/sq/controller/jbxxtxController.java
+++ src/main/java/com/tianxia/sq/controller/jbxxtxController.java
@@ -59,7 +59,7 @@ public class jbxxtxController  {
         System.out.println("保存个人基本信息测试：："+grxx);
         grxxService.updategrxb(grxx);
         ModelAndView mav = new ModelAndView();
-        mav.setViewName("personalWeb/ryxx/gr_ryxx/你好吗？");
+        mav.setViewName("personalWeb/ryxx/gr_ryxx哈哈");
         toryxx(grxx,request,map);
         return mav;
     }
